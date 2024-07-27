6f9b68e0f9d09cc6ba2ac47e4192106cbcf69895
Ahmed <ahmed@ahmed>
2024-07-27 06:55:10 +0300

    fixing typos

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -11,19 +11,7 @@ export default class EVCar extends Car {
 
   // Methods
 
-  cloneCar() {
-    const NewObj = this.constructor[Symbol.species] || this.constructor;
-    const clone = new NewObj();
-    return clone;
+  static get [Symbol.species]() {
+    return Car;
   }
-
-  // Setters
-
-  // Getters
 }
-/* class TestCar extends Car {};
-const tc1 = new TestCar("Nissan", "Turbo", "Pink");
-const tc2 = tc1.cloneCar();
-console.log(tc1 instanceof TestCar);
-console.log(tc2 instanceof TestCar);
-console.log(tc1 == tc2); */
