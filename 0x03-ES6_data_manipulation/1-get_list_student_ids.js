fee7046a217212b75d87a85df4d5ce4ce04f6064
Ahmed <ahmed@ahmed>
2024-07-29 23:10:07 +0300

    3rd task

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,4 @@
-export default function getListStudents(arrarg) {
+export default function getListStudentIds(arrarg) {
     if (Array.isArray(arrarg)) {     
       const idarr = arrarg.map(obj => obj.id);
       return idarr;
---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,4 @@
+export default function getStudentsByLocation(arrarg, city) {
+  const filterarr = arrarg.filter(ind => ind.location === city);
+  return filterarr;
+}
