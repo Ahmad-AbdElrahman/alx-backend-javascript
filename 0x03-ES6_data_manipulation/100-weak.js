af08db1b5c63d034a6e301165ca1fdffc0e93c9f
Ahmed <ahmed@ahmed>
2024-07-31 00:10:07 +0300

    tasks

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,5 +2,5 @@ export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   const currentCount = weakMap.get(endpoint) || 0;
   weakMap.set(endpoint, currentCount + 1);
-  if (currentCount >= 5) throw Error('Endpoint load is high');
+  if (currentCount >= 5) throw new Error('Endpoint load is high');
 }
